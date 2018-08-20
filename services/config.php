65358030b1b74fe3d93f9ff4e4172e7b73fa9e21
Pablo Sepúlveda <psepulveda@fiusoft.cl>
2018-08-20 01:53:40 -0300

    agregado evaluacion3

--- services/config.php
+++ services/config.php
@@ -1,8 +1,8 @@
 <?php
 
 define("DB_HOSTNAME", "localhost");
-define("DB_USERNAME", "desarro6_psep");
-define("DB_PASSWORD", "c11s4.2018");
-define("DB_NAME", "desarro6_sepulveda");
+define("DB_USERNAME", "example");
+define("DB_PASSWORD", "example");
+define("DB_NAME", "example");
 
 ?>
